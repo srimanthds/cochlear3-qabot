d7d08ee7f22306be324afd4676358100f5ee029b
srimanth d s <srimanthdudi95@gmail.com>
2023-12-13 18:20:02 +0000

    RAG Critique

--- Hello.py
+++ Hello.py
@@ -291,7 +291,7 @@ else:
                     st.session_state.qa_data['question'] = query_text
                     st.session_state.qa_data['responses'].append(response)
                     for idx, r in enumerate(st.session_state.qa_data['responses'][::-1], start=1):
-                        st.info(f"Question: {query_text} \nRAG Response : {rag_response}\nResponse : {r} ")
+                        st.info(f"Question: {query_text} \n\n RAG Response : {rag_response} \n\n Response : {r} \n\n")
                         # st.info(f"RAG Response : {rag_response}")
                         # st.info(f"Response : {r}")
 
