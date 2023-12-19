92797321ab8a0af0d214d3082850633057f0e6f0
Tharun Tej Reddy Thodimi <64968224+tharunthodimi@users.noreply.github.com>
2023-12-20 00:47:05 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -376,7 +376,8 @@ else:
                     st.session_state.qa_data['question'] = query_text
                     st.session_state.qa_data['responses'].append(response)
                     for idx, r in enumerate(st.session_state.qa_data['responses'][::-1], start=1):
-                        st.info(f"Question: {query_text} \n\n {rag_response} \n\n")
+                        # st.info(f"Question: {query_text} \n\n {rag_response} \n\n")
+                        st.markdown(f"""**Question:** {query_text}\n {rag_response}""")
                         # st.info(f"Question: {query_text} \n\n {rag_response} \n\n")
                         #st.info(f"Question: {query_text} \n\n {rag_response} \n\n Response : {r} \n\n")
                         
