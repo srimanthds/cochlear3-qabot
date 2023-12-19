6dc0181cda60ae9ff9c6337bed81c2b09aa9e2d4
Tharun Tej Reddy Thodimi <64968224+tharunthodimi@users.noreply.github.com>
2023-12-19 20:28:09 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -122,7 +122,7 @@ The Nucleus CI500 series and Freedom (CI24RE) implants are validated to withstan
 Recipients should seek medical advice before participating in a dive for conditions that might make diving contraindicated, e.g. middle ear infection, etc.
 When wearing a mask avoid pressure over the implant site''',
             "A":"Reasoning- According to source information Sound processors are specified to withstand pressure at a depth of 40m under water for the purposes of scuba diving" \
-            +"you are willing to do diving to 60 meters for sound processors,since 60 meters >40 meters where 40 meters is the maximum withstandable pressure for This device as per the souce information. It is not recommended"
+            +"you are willing to do diving to 60 meters for sound processors,since 60 meters >40 meters where 40 meters is the maximum withstandable pressure for this device as per the souce information. It is not recommended"
             ANSWER- YES".'''
   directions=''' "The response should match the information from context and no external data should be used for generating response",
                 "call center agent question may contain numerical fields in it. If yes, then compare numeric values with thresold values available in context and validate it twice before giving response",
